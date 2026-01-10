a9e480285dfa0c41d46a74d78fa43529b04ba212
Григорий <shmel71rus1@gmail.com>
2026-01-10 13:02:24 +0300

    added the Validator class, added class descriptions, more logging, refactoring all functions using a new class

--- logger.py
+++ logger.py
@@ -2,36 +2,39 @@ import logging
 import os
 import datetime
 
-"""Модуль для логирования проекта"""
-
 
 def create_logger():
     try:
+
         date = datetime.datetime.now().strftime("%Y_%m_%d")
         log_dir = "log"
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
 
-        message_format = logging.Formatter(
-            '%(asctime)s | %(levelname)s | %(name)s | %(funcName)s | %(message)s'
-        )
         logger = logging.getLogger("main_log")
-        # logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
+        logger.propagate = False
+
+        logger.handlers.clear()
+
+        formatter = logging.Formatter(
+            '%(asctime)s | %(levelname)s | %(name)s | %(funcName)s | %(message)s',
+            datefmt='%Y_%m_%d %H:%M:%S'
+        )
 
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(message_format)
-        logger.addHandler(console_handler)
+        console_handler.setFormatter(formatter)
+        # logger.addHandler(console_handler)
 
-        file_handler = logging.FileHandler(
-            f"{log_dir}/{date}.log",
-            encoding="utf-8",
-            mode="a"
-        )
+        file_path = os.path.join(log_dir, f"{date}.log")
+        file_handler = logging.FileHandler(file_path, encoding="utf-8", mode="a")
         file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(message_format)
+        file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
         return logger
+
     except Exception as e:
-        logging.error(e)
+        print(f"Ошибка при создании логгера: {e}")
+        return None
